dc27b4b87be26be6acf77d1f0564d8a9947aefb5
Jason McCoy <jasonmccoy@Jasons-MacBook-Pro.local>
2016-08-27 12:43:28 -0700

    Added all foods
    Finished adding all foods that are good and bad for dogs.

--- CandySearch/MasterViewController.swift
+++ CandySearch/MasterViewController.swift
@@ -41,19 +41,152 @@ class MasterViewController: UITableViewController {
     searchController.dimsBackgroundDuringPresentation = false
     
     // Setup the Scope Bar
-    searchController.searchBar.scopeButtonTitles = ["All", "Chocolate", "Hard", "Other"]
+    searchController.searchBar.scopeButtonTitles = ["All", "Bad for Dogs", "Good for Dogs"]
     tableView.tableHeaderView = searchController.searchBar
     
+    // Manage all the different objects for your users to search.
     candies = [
-      Candy(category:"Chocolate", name:"Chocolate Bar"),
-      Candy(category:"Chocolate", name:"Chocolate Chip"),
-      Candy(category:"Chocolate", name:"Dark Chocolate"),
-      Candy(category:"Hard", name:"Lollipop"),
-      Candy(category:"Hard", name:"Candy Cane"),
-      Candy(category:"Hard", name:"Jaw Breaker"),
-      Candy(category:"Other", name:"Caramel"),
-      Candy(category:"Other", name:"Sour Chew"),
-      Candy(category:"Other", name:"Gummi Bear")]
+        // Can cause intoxication, seizures, low blood sugar, arrhythmias,coma, and death.
+        Candy(category:"Bad for Dogs", name:"Alcoholic beverages"),
+        // The leaves, seeds, fruit, and bark contain persin, which can cause vomiting and diarrhea.
+        Candy(category:"Bad for Dogs", name:"Avocado"),
+        // Can cause obstruction or laceration of the digestive system.
+        Candy(category:"Bad for Dogs", name:"Bones from fish"),
+        // Can cause obstruction or laceration of the digestive system.
+        Candy(category:"Bad for Dogs", name:"Bad for Dogs Bar"),
+        // Can cause obstruction or laceration of the digestive system.
+        Candy(category:"Bad for Dogs", name:"Poultry"),
+        // Generally too high in protein and fats.
+        Candy(category:"Bad for Dogs", name:"Cat food"),
+        // Contain caffeine, theobromine, or theophylline, which can cause vomiting and diarrhea and be toxic to the heart and nervous systems.
+        Candy(category:"Bad for Dogs", name:"Chocolate"),
+        // Contain caffeine, theobromine, or theophylline, which can cause vomiting and diarrhea and be toxic to the heart and nervous systems.
+        Candy(category:"Bad for Dogs", name:"Coffee"),
+        // Contain caffeine, theobromine, or theophylline, which can cause vomiting and diarrhea and be toxic to the heart and nervous systems.
+        Candy(category:"Bad for Dogs", name:"Tea"),
+        // Can cause vomiting.
+        Candy(category:"Bad for Dogs", name:"Citrus oil extracts"),
+        // Can cause pancreatitis.
+        Candy(category:"Bad for Dogs", name:"Fat trimmings"),
+        // If fed exclusively or in high amounts can result in a thiamine (a B vitamin) deficiency leading to loss of appetite, seizures, and in severe cases, death.
+        Candy(category:"Bad for Dogs", name:"Fish (raw, canned or cooked)"),
+        // Contain an unknown toxin, which can damage the kidneys. There have been no problems associated with grape seed extract.
+        Candy(category:"Bad for Dogs", name:"Grapes, raisins and currants"),
+        // Contain an unknown toxin, which can damage the kidneys. There have been no problems associated with grape seed extract.
+        Candy(category:"Bad for Dogs", name:"Raisins"),
+        // Contain an unknown toxin, which can damage the kidneys. There have been no problems associated with grape seed extract.
+        Candy(category:"Bad for Dogs", name:"Currants"),
+        // Unknown compound causes panting, increased heart rate, elevated temperature, seizures, and death.
+        Candy(category:"Bad for Dogs", name:"Hops"),
+        // Can damage the lining of the digestive system and be toxic to the other organs including the liver and kidneys.
+        Candy(category:"Bad for Dogs", name:"Human vitamin supplements containing iron"),
+        // Contain an unknown toxin, which can affect the digestive and nervous systems and muscle.
+        Candy(category:"Bad for Dogs", name:"Macadamia nuts"),
+        // Can depress the nervous system, cause vomiting, and changes in the heart rate.
+        Candy(category:"Bad for Dogs", name:"Marijuana"),
+        // Some adult dogs and cats may develop diarrhea if given large amounts of dairy products.
+        Candy(category:"Bad for Dogs", name:"Milk"),
+        // Some adult dogs and cats may develop diarrhea if given large amounts of dairy products.
+        Candy(category:"Bad for Dogs", name:"Dairy products"),
+        // Can contain multiple toxins causing vomiting and diarrhea and can also affect other organs.
+        Candy(category:"Bad for Dogs", name:"Moldy or spoiled food"),
+        // Can contain toxins, which may affect multiple systems in the body, cause shock, and result in death.
+        Candy(category:"Bad for Dogs", name:"Mushrooms"),
+        // Contain sulfoxides and disulfides, which can damage red blood cells and cause anemia. Cats are more susceptible than dogs.
+        Candy(category:"Bad for Dogs", name:"Onions (raw, cooked, or powder)"),
+        // Contain sulfoxides and disulfides, which can damage red blood cells and cause anemia. Cats are more susceptible than dogs.
+        Candy(category:"Bad for Dogs", name:"Garlic (raw, cooked, or powder)"),
+        // Seeds can cause intestinal obstruction and enteritis.
+        Candy(category:"Bad for Dogs", name:"Persimmons"),
+        // Can cause obstruction of the digestive tract.
+        Candy(category:"Bad for Dogs", name:"Pits (from peaches and plums)"),
+        // Contain an enzyme called avidin, which decreases the absorption ofbiotin (a B vitamin). This can lead to skin and hair coat problems as well as neurologic abnormalities. Raw eggs may also contains Salmonella.
+        Candy(category:"Bad for Dogs", name:"Raw eggs"),
+        // May contain bacteria such as Salmonella and E. coli, which can cause vomiting and diarrhea.
+        Candy(category:"Bad for Dogs", name:"Raw meat"),
+        // Contain oxalates, which can affect the digestive, nervous, and urinary systems.
+        Candy(category:"Bad for Dogs", name:"Rhubarb leaves"),
+        // If eaten in large quantities it may lead to electrolyte imbalances, seizures, and even death.
+        Candy(category:"Bad for Dogs", name:"Salt"),
+        // Can become trapped in the digestive system; called a "string foreign body."
+        Candy(category:"Bad for Dogs", name:"String"),
+        // Can lead to obesity, dental problems, and possibly diabetes mellitus.
+        Candy(category:"Bad for Dogs", name:"Sugary foods"),
+        // Table scraps are not nutritionally balanced. They should never be more than 10% of the diet. Fat should be trimmed from meat; bones should not be fed.
+        Candy(category:"Bad for Dogs", name:"Table food/scraps (in large amounts)"),
+        // Contains nicotine, which affects the digestive and nervous systems. Can result in rapid heart beat, collapse, coma, and death.
+        Candy(category:"Bad for Dogs", name:"Tobacco"),
+        // Can expand and produce gas in the digestive system, causing pain and possible rupture of the stomach or intestines.
+        Candy(category:"Bad for Dogs", name:"Yeast dough"),
+        // Can cause very low blood sugar (hypoglycemia), which can result in vomiting, weakness and collapse. In high doses can cause liver failure and death.
+        Candy(category:"Bad for Dogs", name:"Xylitol (artificial sweetener)"),
+        // Cinnamon and its oils can irritate the inside of pets’ mouths, making them uncomfortable and sick. It can lower a dog’s blood sugar too much and can lead to diarrhea, vomiting, increased, or decreased heart rate and even liver disease. If they inhale it in powder form, cinnamon can cause difficulty breathing, coughing, and choking.
+        Candy(category:"Bad for Dogs", name:"Cinnamon"),
+        // Like cherries, the seeds, leaves and stems of apricot plants are toxic to dogs. While they are able to consume the pulp of the fruit with no ill effects, caution should be taken if your dog has access to any other parts of the plant. These plants also contain cyanide, and can result in respiratory failure and death.
+        Candy(category:"Bad for Dogs", name:"Apricots"),
+        // Nutmeg can also be stored in the pantry with other potentially hazardous substances for pets. Often used as a spice for baking, nutmeg's rich, spicy scent is attractive to dogs. High levels can be fatal. Signs include tremors, seizures and nervous system abnormalities.
+        Candy(category:"Bad for Dogs", name:"Nutmeg"),
+        // Cat food contains proteins and fats that are targeted at the diet of a cat, not a dog. The protein and fat levels in cat food are too high for your dog, and not healthy.
+        Candy(category:"Bad for Dogs", name:"Baby food"),
+        // Gastrointestinal irritant.
+        Candy(category:"Bad for Dogs", name:"Broccoli"),
+        // Peanut Butter is one of the best treats to give to dogs because it lasts them so long! Plus, its packed full of protein, healthy fats, niacin, vitamin B and vitamin E.
+        Candy(category:"Good for Dogs", name:"Peanut Butter"),
+        // Cooked chicken meat is a perfectly suitable snack or meal additive
+        Candy(category:"Good for Dogs", name:"Chicken"),
+        // Carrots are high in fiber and vitamin A while being low in calories.
+        Candy(category:"Good for Dogs", name:"Carrots"),
+        // Yogurt is full of protein, calcium and digestive cultures and is an excellent way to improve your pup’s digestive health.
+        Candy(category:"Good for Dogs", name:"Yogurt"),
+        // Canned pumpkin or fresh, cooked pumpkin with no added sugars and spices is a great choice for dogs with a sensitive stomach. It’s also an excellent source of vitamin A and fiber.
+        Candy(category:"Good for Dogs", name:"Pumpkin"),
+        // Green beans are highly recommended by veterinarians for owners looking to help their dogs loose weight. They are very high in fiber but low in calories, making them a healthy treat alternative that’s filling but won’t add any weight.
+        Candy(category:"Good for Dogs", name:"Green Beans"),
+        // Salmon is very high in health omega-3 fatty acids and is typically the fish used to make fish oils for our pets.
+        Candy(category:"Good for Dogs", name:"Salmon"),
+        // Sweet potatoes work similarly to pumpkin as they are high in vitamin A, fiber and other nutrients. They are easily digestible when steamed or baked, served unseasoned.
+        Candy(category:"Good for Dogs", name:"Sweet Potatoes"),
+        // Sliced apples are a healthy and tasty treat for dogs that are full of phytonutrients, vitamin A and vitamin C. They can be given with the skin on, but avoid feeding the seeds as they naturally contain cyanide.
+        Candy(category:"Good for Dogs", name:"Apples"),
+        // Oatmeal is found in many dog foods and for those not sensitive to grains, it can be a healthy additive to your dog’s meal. Not only is it packed with vitamins and minerals, it’s an excellent source of dietary fiber.
+        Candy(category:"Good for Dogs", name:"Oatmeal"),
+        // Eggs are a great source of very digestible protein, riboflavin, and selenium. - See more at: http://moderndogmagazine.com/articles/10-people-foods-dogs/1896#sthash.XNuKqmf7.dpuf
+        Candy(category:"Good for Dogs", name:"Eggs"),
+        /*//
+        Candy(category:"Bad for Dogs", name:"Caramel"),
+        //
+        Candy(category:"Bad for Dogs", name:"Sour Chew"),
+        //
+        Candy(category:"Bad for Dogs", name:"Gummi Bear"),
+        //
+        Candy(category:"Bad for Dogs", name:"Bad for Dogs Bar"),
+        //
+        Candy(category:"Bad for Dogs", name:"Bad for Dogs Chip"),
+        //
+        Candy(category:"Bad for Dogs", name:"Dark Bad for Dogs"),
+        //
+        Candy(category:"Bad for Dogs", name:"Lollipop"),
+        //
+        Candy(category:"Bad for Dogs", name:"Candy Cane"),
+        //
+        Candy(category:"Bad for Dogs", name:"Jaw Breaker"),
+        //
+        Candy(category:"Bad for Dogs", name:"Caramel"),
+        //
+        Candy(category:"Bad for Dogs", name:"Sour Chew"),
+        //
+        Candy(category:"Bad for Dogs", name:"Gummi Bear"),
+        //
+        Candy(category:"Bad for Dogs", name:"Bad for Dogs Bar"),
+        //
+        Candy(category:"Bad for Dogs", name:"Bad for Dogs Chip"),
+        //
+        Candy(category:"Bad for Dogs", name:"Dark Bad for Dogs"),
+        //
+        Candy(category:"Bad for Dogs", name:"Lollipop"),
+        //
+        Candy(category:"Bad for Dogs", name:"Candy Cane") */
+              ]
     
     if let splitViewController = splitViewController {
       let controllers = splitViewController.viewControllers
